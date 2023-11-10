9c83e3b9d675cb74aa20be7d24aafb4a3f7abdf2
Dalmar Duraes <dalmar.duraes@gmail.com>
2023-11-10 01:53:24 -0300

    Add Player Movement

--- Blue Gravity Sky Task/Assets/Scripts/Player/PlayerMovement.cs
+++ Blue Gravity Sky Task/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,21 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 namespace BlueGravityStudios
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerMovement : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private float _speed;
+        private PlayerInput _playerInput;
+        private Rigidbody2D _rb2D;
+        private PlayerInputAction playerInputAction;
+        private Vector2 _inputVector;
+        private void Awake()
         {
-
+            _playerInput = GetComponent<PlayerInput>();
+            _rb2D = GetComponent<Rigidbody2D>();
+            playerInputAction = new PlayerInputAction();
+            playerInputAction.PlayerActionMap.Enable();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FixedUpdate()
         {
-
+            _inputVector = playerInputAction.PlayerActionMap.PlayerMovement.ReadValue<Vector2>();
+            _rb2D.velocity = _inputVector * _speed;
         }
+
+    
+        
+        
+        
     }
 }
