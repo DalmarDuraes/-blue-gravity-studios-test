dbe9a9b6cedcc7f24899bc3d50bd6b8652656bb9
Dalmar Duraes <dalmar.duraes@gmail.com>
2023-11-09 22:03:42 -0300

    Improve Shop UI

--- Blue Gravity Sky Task/Assets/Scripts/Item/Item.cs
+++ Blue Gravity Sky Task/Assets/Scripts/Item/Item.cs
@@ -18,6 +18,7 @@ namespace BlueGravityStudios
         protected int _itemSellPrice;
         protected Sprite _itemSprite;
         protected Sprite _secondaryItemSprite;
+        protected Sprite _spriteForPanels;
         protected ItemType _itemType;
 
         public ItemScriptable ItemScriptable => _itemScriptable;
@@ -25,6 +26,7 @@ namespace BlueGravityStudios
         public int ItemSellPrice =>_itemSellPrice;
         public Sprite ItemSprite => _itemSprite;
         public Sprite SecondaryItemSprite => _secondaryItemSprite;
+        protected Sprite SpriteForPanels => _spriteForPanels;
         public ItemType ItemType => _itemType;
 
         public void CallInit()
@@ -36,13 +38,14 @@ namespace BlueGravityStudios
         {
             _itemSprite = _itemScriptable.ItemSprite;
             _secondaryItemSprite = _itemScriptable.SecondaryItemSprite;
+            _spriteForPanels = _itemScriptable.SpriteForPanels;
             _itemName = _itemScriptable.ItemName;
             _itemPrice = _itemScriptable.ItemPrice;
             _itemType = _itemScriptable.ItemType;
             _itemNameTxt.text = _itemName;
             _itemPriceTxt.text = _itemPrice.ToString();
             _itemSellPrice = _itemPrice / 2;
-            _itemSpriteOnUi.sprite = _itemSprite;
+            _itemSpriteOnUi.sprite = _spriteForPanels;
         }
         
         public void SetItemScriptable(ItemScriptable itemScriptable) => _itemScriptable = itemScriptable;
